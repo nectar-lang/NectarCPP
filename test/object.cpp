8244102093445d63f13481f20c45c4606e915898
Adrien THIERRY <adrien.thierryfr@gmail.com>
2020-12-03 13:35:29 +0100

    Update object.cpp example/test with new ways of init an object

--- test/object.cpp
+++ test/object.cpp
@@ -19,7 +19,9 @@ int main(int argc, char** argv)
 		{"one", 1}, // number
 		{"two", "2"}, // string
 		{"three", {1,2,3}}, // array
-		{"four", new Object("key", "value")} // sub object with only one key
+		{"four", new Object("key", "value")}, // sub object with only one key
+		{"five", new Object({"key", 1})}, // another way with non string value
+		{"six", new Object({ {"key", 2} })}, // another way with non string value
 	});
 	println(init);
 
